6e500a50f8243ca441f77592c768cf281a4f38fe
Fabian Eduardo Estrella Romero <festrella@aeromexico.com>
2024-09-18 14:10:02 -0600

    add enum for http status and http res

--- enums/http.js
+++ enums/http.js
@@ -7,3 +7,13 @@ export const HttpStatus = Object.freeze({
   NOT_FOUND: 404,
   INTERNAL_SERVER_ERROR: 500,
 });
+
+export const HttpResponseMessage = Object.freeze({
+  SUCCESS: "Success",
+  CREATED: "Resource created successfully",
+  BAD_REQUEST: "Bad request",
+  UNAUTHORIZED: "Unauthorized access",
+  FORBIDDEN: "Access forbidden",
+  NOT_FOUND: "Resource not found",
+  SERVER_ERROR: "Internal server error",
+});
